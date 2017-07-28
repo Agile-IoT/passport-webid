1d9e1f055be371d97cb75ed539a0abd51a7a27ea
Philippe Krief <philippe.krief@eclipse.org>
2017-07-28 18:01:55 +0200

    Providing the correct ownership
    Who is Baptiste Lafontaine?

--- examples/login/db.js
+++ examples/login/db.js
@@ -1,12 +1,12 @@
 /*******************************************************************************
- *Copyright (C) 2017 FBK.
+ *Copyright (C) 2017 UNI Passau.
  *All rights reserved. This program and the accompanying materials
  *are made available under the terms of the Eclipse Public License v1.0
  *which accompanies this distribution, and is available at
  *http://www.eclipse.org/legal/epl-v10.html
  *
  *Contributors:
- *    FBK - initial API and implementation
+ *    UNI Passau - initial API and implementation
  ******************************************************************************/
 /*
 modified from https://github.com/jaredhanson/oauth2orize/blob/master/examples/all-grants/db/users.js
